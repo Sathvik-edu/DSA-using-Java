01d55ed08c4856da2780f6c21fec3e53e2529af0
Sathvik-edu <sathvikswamykm.edu@gmail.com>
2023-09-28 13:20:33 +0530

    Graphs

---
+++ src/DataStructures/Graphs/Main.java
@@ -0,0 +1,94 @@
+package DataStructures.Graphs;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*    //----ADD VERTEX METHOD----//
+        Graph myGraph = new Graph();
+
+        myGraph.addVertex("A");
+        myGraph.addVertex("B");
+
+        System.out.println("Graph:");
+        myGraph.printGraph();      */
+
+
+
+
+        /*    //----ADD EDGE METHOD----//
+        Graph myGraph = new Graph();
+
+        myGraph.addVertex("A");
+        myGraph.addVertex("B");
+
+
+        System.out.println("Graph before addEdge():");
+        myGraph.printGraph();
+
+
+        myGraph.addEdge("A", "B");
+
+
+        System.out.println("\nGraph after addEdge():");
+        myGraph.printGraph();             */
+
+
+
+
+
+
+
+        /*    //----REMOVE EDGE METHOD----//
+        Graph myGraph = new Graph();
+
+        myGraph.addVertex("A");
+        myGraph.addVertex("B");
+        myGraph.addVertex("C");
+
+        myGraph.addEdge("A", "B");
+        myGraph.addEdge("A", "C");
+        myGraph.addEdge("B", "C");
+
+
+        System.out.println("Graph before removeEdge():");
+        myGraph.printGraph();
+
+        myGraph.removeEdge("A", "B");
+
+        System.out.println("\nGraph after removeEdge():");
+        myGraph.printGraph();              */
+
+
+
+
+
+
+
+
+
+        /*    //----REMOVE VERTEX METHOD----//
+        Graph myGraph = new Graph();
+
+        myGraph.addVertex("A");
+        myGraph.addVertex("B");
+        myGraph.addVertex("C");
+        myGraph.addVertex("D");
+
+        myGraph.addEdge("A", "B");
+        myGraph.addEdge("A", "C");
+        myGraph.addEdge("A", "D");
+        myGraph.addEdge("B", "D");
+        myGraph.addEdge("C", "D");
+
+
+        System.out.println("Graph before removeVertex():");
+        myGraph.printGraph();
+
+        myGraph.removeVertex("D");
+
+        System.out.println("\nGraph after removeVertex():");
+        myGraph.printGraph();                     */
+    }
+
+}
